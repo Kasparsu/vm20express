32b233f5082f8b6e2c23a1950cfaad557f710dee
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-02-07 12:48:16 +0200

    add posts and users

---
+++ controllers/PostController.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let router  = express.Router();
+
+router.get('/posts', (req, res) => {
+    let json = fs.readFileSync(path.join(__dirname, 'data/posts.json'));
+    let posts = JSON.parse(json);
+    res.render('posts.html', {posts}); // same as {posts: posts}
+});
+  
+router.get('/posts/new', (req, res) => {
+    res.render('newpost.html'); 
+});
+  
+router.post('/posts/new', (req, res) => {
+    let json = fs.readFileSync(path.join(__dirname, 'data/posts.json'));
+    let posts = JSON.parse(json);
+    posts.push(req.body);
+    json = JSON.stringify(posts);
+    fs.writeFileSync(path.join(__dirname, 'data/posts.json'), json);
+    res.redirect('/posts');
+});
+  
+router.get('/posts/:id/edit', (req, res) => {
+    let json = fs.readFileSync(path.join(__dirname, 'data/posts.json'));
+    let posts = JSON.parse(json);
+    let post = posts[req.params.id];
+    post.id = req.params.id;
+    res.render('editpost.html', {post}); 
+});
+  
+router.post('/posts/:id', (req, res) => {
+    let json = fs.readFileSync(path.join(__dirname, 'data/posts.json'));
+    let posts = JSON.parse(json);
+    posts[req.params.id] = req.body;
+    json = JSON.stringify(posts);
+    fs.writeFileSync(path.join(__dirname, 'data/posts.json'), json);
+    res.redirect('/posts');
+});
+  
+router.get('/posts/:id/delete', (req, res) => {
+    let json = fs.readFileSync(path.join(__dirname, 'data/posts.json'));
+    let posts = JSON.parse(json);
+    posts.splice(req.params.id, 1);
+    json = JSON.stringify(posts);
+    fs.writeFileSync(path.join(__dirname, 'data/posts.json'), json);
+    res.redirect('/posts');
+});
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const nunjucks = require('nunjucks')
+const bcrypt = require('bcryptjs');
 const app = express()
 const port = 3000
 
@@ -31,24 +32,32 @@ app.get('/greeting', (req, res) => {
     res.render('greeting.html', {name: req.query.name, age: req.query.age})
 })
 
-app.get('/posts', (req, res) => {
-  let json = fs.readFileSync(path.join(__dirname, 'data/posts.json'));
-  let posts = JSON.parse(json);
-  res.render('posts.html', {posts}) // same as {posts: posts}
+
+
+app.get('/register', (req, res) => {
+  res.render('register.html') 
 })
 
-app.get('/posts/new', (req, res) => {
-  res.render('newpost.html') 
-})
-
-app.post('/posts/new', (req, res) => {
-  let json = fs.readFileSync(path.join(__dirname, 'data/posts.json'));
-  let posts = JSON.parse(json);
-  posts.push(req.body);
-  json = JSON.stringify(posts);
-  fs.writeFileSync(path.join(__dirname, 'data/posts.json'), json);
-  res.redirect('/posts');
+app.post('/register', (req, res) => {
+  let json = fs.readFileSync(path.join(__dirname, 'data/users.json'));
+  let users = JSON.parse(json);
+  
+  req.body.password = crypt(req.body.password);
+  users.push(req.body);
+  json = JSON.stringify(users);
+  fs.writeFileSync(path.join(__dirname, 'data/users.json'), json);
+  res.redirect('/');
 });
+
+function crypt(pass){
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(pass, salt);
+  return hash;
+}
+
+function check(pass, hash){
+  return bcrypt.compareSync(pass, hash);
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
